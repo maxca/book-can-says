d48381cb16f3127d24dd902cf0cf7d8ab7a9a30b
hippiejam <super.chupachup@gmail.com>
2019-05-04 13:03:03 +0700

    add JammController

---
+++ app/Http/Controllers/JammController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class JammController extends Controller
+{
+    public function welcome(){
+        return 'welcome to website';
+    }
+}

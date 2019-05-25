f2d6e3702c6028df213f923d77b4bb7376438ef0
Pimmepim <thitapa.59130500021@mail.kmutt.ac.th>
2019-05-25 22:19:26 +0700

    no message

--- routes/web.php
+++ routes/web.php
@@ -32,25 +32,7 @@ Route::get('recorder', function () {
     return view('recorder.index');
 });
 
-//<<<<<<< HEAD
+
 Route::get('facebook','facebookController@facebookLogin');
 Route::get('callback-url','facebookController@callBack');
-
-
-//=======
-Route::group(['prefix' => 'recorder'], function () {
-    Route::get('/', 'UploadSoundController@index');
-    Route::post('/upload', 'UploadSoundController@upload');
-});
-
-Route::get('facebook', 'facebookController@facebookLogin');
-Route::get('callback-url', 'facebookController@callBack');
-
-
-# social login
-Route::get('login/facebook', 'Auth\FacebookLoginController@redirectToProvider');
-Route::get('login/facebook/callback', 'Auth\FacebookLoginController@handleProviderCallback');
-
-Route::get('login/google', 'Auth\GoogleLoginController@redirectToProvider');
-Route::get('login/google/callback', 'Auth\GoogleLoginController@handleProviderCallback');
 
